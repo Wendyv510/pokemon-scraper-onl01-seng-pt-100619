05c8648a79dd94d0925615a70502a3b66e644090
Wendy Harris <wendyv510@hotmail.com>
2020-01-07 22:58:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id:,name:,type:,db:)
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db
+  end 
+  
+  def self.save(name,type,db)
+    sql = "INSERT INTO pokemon(name,type) VALUES (?,?);",name, type
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.find(id,db)
+    sql = "SELECT * FROM pokemon WHERE id = ?",id
+     pokemon = DB[:conn].execute(sql,db) 
+      name = pokemon[1]
+      type = pokemon[2]
+      hp = pokemon [3]
+   Pokemon.new_from_db(id: id, name: name, type: type, hp: hp, db: database_connection)
+  end 
 end
